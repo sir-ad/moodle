587ac0a86707ab2263dba71f72f8a3064a884163
sir-ad <33547839+sir-ad@users.noreply.github.com>
2020-07-17 14:54:08 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,21 +4,21 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
-$CFG->dbtype    = getenv('DATABASE_TYPE');
+$CFG->dbtype    = getenv('psql');
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = getenv('DATABASE_HOST');
-$CFG->dbname    = getenv('DATABASE_NAME');
-$CFG->dbuser    = getenv('DATABASE_USER');
-$CFG->dbpass    = getenv('DATABASE_PASSWORD');
+$CFG->dbhost    = getenv('ec2-3-215-83-17.compute-1.amazonaws.com');
+$CFG->dbname    = getenv('devscfca42dbt');
+$CFG->dbuser    = getenv('pvdkapietrljpa');
+$CFG->dbpass    = getenv('674ef7246388066e5359fd4ea35475b09d9aec932b06d631c3e4e42e5ca71388');
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
-  'dbport' => getenv('DATABASE_PORT'),
+  'dbport' => getenv('5432'),
   'dbsocket' => '',
 );
 
-$CFG->wwwroot   = getenv('WWWROOT');
-$CFG->dataroot  = getenv('DATAROOT');
+$CFG->wwwroot   = getenv('https://moodlekm.herokuapp.com/');
+$CFG->dataroot  = getenv(try /tmp);
 $CFG->admin     = 'admin';
 
 $CFG->directorypermissions = 0777;
